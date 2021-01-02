42678906dbffce8d9a11d0c4a8dc86b8600d558c
highwolfx <seanchangx@gmail.com>
2021-01-01 17:25:21 -0800

    Added 1st level menu options

--- index.js
+++ index.js
@@ -8,3 +8,40 @@ connection.connect(function(err) {
     if (err) throw err;
     start();
 });
+
+function start() {
+    inquirer.prompt({
+        name: "query",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ['View', 'Add/Delete', 'Edit Existing', 'Exit']
+    })
+    .then(function(answer) {
+        switch (answer){
+            case 'View':
+                viewOptions();
+                break;
+            case 'Add/Delete':
+                addOptions();
+                break;
+            case 'Edit Existing':
+                editOptions();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        };
+    });
+};
+
+function viewOptions() {
+    
+};
+
+function addOptions() {
+
+};
+
+function editOptions() {
+
+};
